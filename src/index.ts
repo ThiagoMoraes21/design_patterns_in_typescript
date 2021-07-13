13893b9c2f0ee26c998e6e5bffb8154dbba55f8e
Thiago Moraes <thiago4x70@gmail.com>
2021-07-13 16:23:00 -0300

    add ts-node to the project, so I can run typescript with node

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,36 @@
-alert('Hello World');
+interface Pokemon {
+    id: string;
+    attack: number;
+    defense: number;
+}
+
+interface BaseRecord {
+    id: string;
+}
+
+interface Database<T extends BaseRecord> {
+    set(newValue: T): void;
+    get(id: string): T | undefined;
+}
+
+class InMemoryDatabase<T extends BaseRecord> implements Database<T> {
+    private db: Record<string, T> = {};
+
+    public set(newValue: T) {
+        this.db[newValue.id] = newValue;
+    }
+
+    public get(id: string): T | undefined {
+        return this.db[id];
+    }
+}
+
+const pokemonDB = new InMemoryDatabase<Pokemon>();
+pokemonDB.set({
+    id: 'Bulbasaur',
+    attack: 50,
+    defense: 10
+});
+
+console.log(pokemonDB.get('Bulbasaur'));
+
